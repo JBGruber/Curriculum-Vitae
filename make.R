a2d695a667428aebf037c637c1ab908872c5e548
JBGruber <JohannesB.Gruber@gmail.com>
2023-10-04 14:38:15 +0200

    adds download counts

--- make.R
+++ make.R
@@ -66,5 +66,33 @@ make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
               "pdf_version/CV_JohannesGruber.pdf")
   rmarkdown::render("README.Rmd")
 }
+
+update_downloads <- function(file = "CV_JohannesGruber.bib",
+                             packages = c("askgpt", "rwhatsapp", "LexisNexisTools")) {
+  
+  if (file.exists(".last_updated")) 
+    if (readLines(".last_updated") >= Sys.Date())
+      return(TRUE)    
+  
+  lines <- readLines(file)
+  entry_start <- grep("@.+?\\{", lines)
+  ends <- c(entry_start[2:length(entry_start)] - 1, length(lines))
+  entries <- lapply(seq_along(entry_start), function(i) lines[entry_start[i]:ends[i]])
+  
+  for (i in seq_along(packages)) {
+    ds <- cranlogs::cran_downloads(packages[i], from = "2018-04-09", to = Sys.Date())$count |> 
+      sum() |> 
+      scales::comma()
+    
+    entry <- entries[[grep(packages[i], entries)]]
+    entries[[grep(packages[i], entries)]] <- gsub("\\{Cranlogs download count: [0-9,]+\\}",
+                                                  paste("{Cranlogs download count:", ds, "}"),
+                                                  entry)
+  }
+  writeLines(as.character(Sys.Date()), ".last_updated")
+  writeLines(unlist(entries), file)
+  return(TRUE)    
+}
+update_downloads()
 make_cv()
 
