22cbac1c176bfd9ab64a35ddf22057e5d93fb611
JBGruber <JohannesB.Gruber@gmail.com>
2026-02-23 16:37:05 +0100

    add doi to packages

--- make.R
+++ make.R
@@ -1,6 +1,5 @@
 # run with: Rscript make.R
 make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
-  
   x <- spelling::spell_check_files(
     path = file,
     ignore = readLines("DICTIONARY"),
@@ -12,7 +11,7 @@ make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
     message(
       "The following spelling errors were found."
     )
-    
+
     print(x)
     if (interactive()) {
       message(
@@ -22,7 +21,7 @@ make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
     } else {
       choice <- 0
     }
-    
+
     if (choice == 1L) {
       dict <- sort(c(readLines("DICTIONARY"), x$word))
       writeLines(dict, "DICTIONARY")
@@ -30,14 +29,14 @@ make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
       stop("Spelling errors found.")
     }
   }
-  
+
   if (clean) {
     unlink(c("pdf_version", "png_version"), recursive = TRUE)
   }
 
   dir.create("pdf_version")
   knitr::knit(file)
-  
+
   check <- try(tinytex::latexmk(
     paste0(tools::file_path_sans_ext(file), ".tex"),
     engine = "xelatex",
@@ -46,7 +45,7 @@ make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
     install_packages = FALSE
   ))
   missing <- character(1L)
-  
+
   while (class(check) == "try-error" && length(missing) > 0) {
     check <- try(tinytex::latexmk(
       paste0(tools::file_path_sans_ext(file), ".tex"),
@@ -55,56 +54,71 @@ make_cv <- function(file = "CV_JohannesGruber.Rnw", clean = TRUE) {
       clean = FALSE,
       install_packages = FALSE
     ))
-    missing <- tinytex::parse_packages(paste0(tools::file_path_sans_ext(file), ".log"))
+    missing <- tinytex::parse_packages(paste0(
+      tools::file_path_sans_ext(file),
+      ".log"
+    ))
     tinytex::tlmgr_install(
       missing
     )
   }
-  
+
   file.remove(list.files(".", ".aux$|.bbl$"))
-  file.rename("CV_JohannesGruber.pdf",
-              "pdf_version/CV_JohannesGruber.pdf")
+  file.rename("CV_JohannesGruber.pdf", "pdf_version/CV_JohannesGruber.pdf")
   rmarkdown::render("README.Rmd")
 }
 
-update_downloads <- function(file = "CV_JohannesGruber.bib",
-                             packages = c("askgpt",
-                                          "atrrr",
-                                          "rwhatsapp", 
-                                          "LexisNexisTools",
-                                          "cookiemonster",
-                                          "rollama",
-                                          "spacyr",
-                                          "quanteda.textmodels")) {
-  
-  if (file.exists(".last_updated")) 
-    if (readLines(".last_updated") >= Sys.Date())
-      return(TRUE)    
-  
+update_downloads <- function(
+  file = "CV_JohannesGruber.bib",
+  packages = c(
+    "askgpt",
+    "atrrr",
+    "rwhatsapp",
+    "LexisNexisTools",
+    "cookiemonster",
+    "rollama",
+    "traktok",
+    "spacyr",
+    "quanteda.textmodels"
+  )
+) {
+  if (file.exists(".last_updated")) {
+    if (readLines(".last_updated") >= Sys.Date()) {
+      return(TRUE)
+    }
+  }
+
   lines <- readLines(file)
   entry_start <- grep("@.+?\\{", lines)
   entry_end <- c(entry_start[2:length(entry_start)] - 1, length(lines))
-  
-  entries <- lapply(seq_along(entry_start), function(i) 
-    lines[entry_start[i]:entry_end[i]])
-  
+
+  entries <- lapply(seq_along(entry_start), function(i) {
+    lines[entry_start[i]:entry_end[i]]
+  })
+
   for (i in seq_along(packages)) {
-    if (interactive()) 
+    if (interactive()) {
       message(packages[i])
-    ds <- cranlogs::cran_downloads(packages[i], from = "2018-04-09", to = Sys.Date())$count |> 
-      sum() |> 
+    }
+    ds <- cranlogs::cran_downloads(
+      packages[i],
+      from = "2018-04-09",
+      to = Sys.Date()
+    )$count |>
+      sum() |>
       scales::comma()
-    
+
     pattern <- paste0("@package{", packages[i])
     entry <- entries[[grep(pattern, entries, fixed = TRUE)]]
-    entries[[grep(pattern, entries, fixed = TRUE)]] <- gsub("\\{Cranlogs download count: [0-9,]+\\}",
-                                                            paste0("{Cranlogs download count: ", ds, "}"),
-                                                            entry)
+    entries[[grep(pattern, entries, fixed = TRUE)]] <- gsub(
+      "\\{Cranlogs download count: [0-9,]+\\}",
+      paste0("{Cranlogs download count: ", ds, "}"),
+      entry
+    )
   }
   writeLines(as.character(Sys.Date()), ".last_updated")
   writeLines(unlist(entries), file)
-  return(TRUE)    
+  return(TRUE)
 }
 update_downloads()
 make_cv()
-
